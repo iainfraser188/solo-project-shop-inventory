e7f8364b9d1d27843e758a5acafdb6fa6a70e426
iain <iainfraser188@msn.com>
2021-11-05 13:41:09 +0000

    inital class setup

---
+++ models/guitar.py
@@ -0,0 +1,9 @@
+class Guitar:
+    def __init__(self,name,manufacturer,type,no_of_strings,production_date,stock_price,selling_price):
+        self.name = name
+        self.manufacturer = manufacturer
+        self.type = type
+        self.no_of_strings = no_of_strings
+        self.production_date = production_date
+        self.stock_price = stock_price
+        self.selling_price = selling_price
---
+++ models/manufacturer.py
@@ -0,0 +1,6 @@
+class Manufacturer:
+    def __init__(self,name,product_name,founded,location):
+        self.name = name
+        self.product_name = product_name
+        self.founded = founded
+        self.location = location
